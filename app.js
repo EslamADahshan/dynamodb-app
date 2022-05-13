cb4dc223395f4c7cc8f57c1c895f5dceadcaefc1
eslamdahshan61 <eslamdahshan61@gmail.com>
2022-05-13 17:00:13 +0200

    dd

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var params = {
 };
 // port on which the server listens
  
-const PORT = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 const client = new AWS.DynamoDB.DocumentClient();
 
 
